672c70b6265d11d8d4bab76e9a70f1b144201769
DheepanDev <dheepdev01@gmail.com>
2020-03-15 10:48:28 +0530

    stateless widget done...next stateful awaits

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-  home: NinjaCard()
-));
+void main() => runApp(MaterialApp(home: NinjaCard()));
 
 class NinjaCard extends StatelessWidget {
   @override
@@ -12,10 +10,71 @@ class NinjaCard extends StatelessWidget {
       appBar: AppBar(
         title: Text('Ninja ID Card'),
         centerTitle: true,
-        backgroundColor:Colors.lightBlue[555],
+        backgroundColor: Colors.lightBlue[555],
         elevation: 0.0,
       ),
-      
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/rajni.jpg'),
+                radius: 40.0,
+                
+              ),
+            ),
+            Divider(
+              height: 90.0,
+              color: Colors.blueGrey[600],
+            ),
+            Text(
+              'NAME',
+              style: TextStyle(color: Colors.blueGrey, letterSpacing: 2.0),
+            ),
+            SizedBox(height: 10.0),
+            Text(
+              'Dheep',
+              style: TextStyle(
+                  color: Colors.amberAccent,
+                  letterSpacing: 2.0,
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 30.0),
+            Text(
+              'NINJA LEVEL',
+              style: TextStyle(color: Colors.blueGrey, letterSpacing: 2.0),
+            ),
+            SizedBox(height: 10.0),
+            Text(
+              '8',
+              style: TextStyle(
+                  color: Colors.amberAccent,
+                  letterSpacing: 2.0,
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 30.0),
+            Row(children: <Widget>[
+              Icon(
+                Icons.email,
+                color: Colors.grey[400],
+              ),
+              SizedBox(width: 10.0),
+              Text(
+                'dheepd@jgjfdk.com',
+                style: TextStyle(
+                  color: Colors.blueAccent[600],
+                  fontSize: 18.0,
+                  letterSpacing: 1.0,
+                ),
+              ),
+            ])
+          ],
+        ),
+      ),
     );
   }
 }
